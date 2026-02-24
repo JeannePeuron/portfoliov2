c404e027bfd0cfda0bf0f0c4ce874bd8d9aa0d2a
JeannePeuron <jeanne.peuron@gmail.com>
2026-02-24 16:29:56 +0100

    feat : cv change

---
+++ src/components/Form.tsx
@@ -0,0 +1,13 @@
+export default function Form() {
+  return (
+    <form action="">
+      <label htmlFor=""></label>
+      <input type="text" />
+      <label htmlFor=""></label>
+      <input type="text" />
+      <label htmlFor=""></label>
+      <input type="text" />
+      <button type="submit "> Envoyer </button>
+    </form>
+  );
+}
